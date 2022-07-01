281e2fe504287048bc2f3f4bf8936329d3a12783
Toknnick <106018075+Toknnick@users.noreply.github.com>
2022-07-02 00:05:05 +0300

    Update Program.cs

--- Task4.5/Program.cs
+++ Task4.5/Program.cs
@@ -10,34 +10,46 @@ namespace Task4._5
             int valueOfRandom = 9;
             int[] arrayOfNumbers = new int[valueOfRandom];
 
-            for (int i = 0; i < arrayOfNumbers.Length;i++)
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
                 arrayOfNumbers[i] = random.Next(valueOfRandom);
 
+            WriteArrayBeforeShuffle(arrayOfNumbers);
             Shuffle(arrayOfNumbers);
+            WriteArrayAftereShuffle(arrayOfNumbers);
         }
 
-        static void Shuffle(int[] array)
+        static void WriteArrayBeforeShuffle(int[] arrayOfNumbers)
+        {
+            Console.WriteLine("Массив:");
+            WriteArray(arrayOfNumbers);
+        }
+
+        static void WriteArray(int[] arrayOfNumbers)
+        {
+            foreach (int number in arrayOfNumbers)
+                Console.Write(number + " ");
+            Console.WriteLine();
+        }
+
+        static void WriteArrayAftereShuffle(int[] arrayOfNumbers)
+        {
+            Console.WriteLine("\nПеремешанный массив:");
+            WriteArray(arrayOfNumbers);
+        }
+
+        static int[] Shuffle(int[] arrayOfNumbers)
         {
             Random random = new Random();
-            Console.WriteLine("Массив:");
 
-            foreach (int number in array)
-                Console.Write(number + " ");
-
-            Console.WriteLine("\nПеремешанный массив:");
-
-            for (int i = array.Length - 1; i > 0; i--)
+            for (int i = arrayOfNumbers.Length - 1; i > 0; i--)
             {
                 int number = random.Next(i);
-                int tempNumber = array[i];
-                array[i] = array[number];
-                array[number] = tempNumber;
+                int tempNumber = arrayOfNumbers[i];
+                arrayOfNumbers[i] = arrayOfNumbers[number];
+                arrayOfNumbers[number] = tempNumber;
             }
 
-            foreach (int number in array)
-                Console.Write(number + " ");
-
-            Console.WriteLine();
+            return arrayOfNumbers;
         }
     }
 }
